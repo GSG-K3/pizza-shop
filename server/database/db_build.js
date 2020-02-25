8c5c7572cc258d6b882e4f808b84e787b8dbc2e4
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-02-25 15:22:24 +0200

    create database connections #5

---
+++ server/database/db_build.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const connection = require('./db_connection');
+
+
+// get data from database
+const getSql = "select * from pizza_order"
+const getData = callback => {
+    connection.query(getSql, (err,res) => {
+    if (err) throw err; 
+    else {callback(err,res.rows)}
+})
+}
+
+// post data to database by using queries from sql file
+const sql = fs.readFileSync(path.join(__dirname,'db_build.sql')).toString();
+
+const postData = callback => {
+    connection.query(sql, (err,res) => {
+    if (err) throw err; 
+    else {callback(err,res.rows)}
+})
+}
+
+
+
+
+
+module.exports = {getData, postData} ;
